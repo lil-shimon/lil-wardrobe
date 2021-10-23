e9876fab2543fc25e834965b9f1e2f6657938021
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-10-23 13:20:45 +0900

    add logout

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:lil_wardrobe/ui/pages/login_page.dart';
 import 'package:lil_wardrobe/ui/pages/signup_page.dart';
 import 'package:lil_wardrobe/ui/pages/verification_page.dart';
 
+import 'ui/widget/organisms/camera_flow.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -38,20 +40,28 @@ class _MyAppState extends State<MyApp> {
           if (snapshot.hasData) {
             return Navigator(
               pages: [
+                /// Loginケース
                 if (snapshot.data!.authFlowStatus == AuthFlowStatus.login)
                   MaterialPage(child: LoginPage(
                     shouldShowSignUp: _authService.showSignUp,
                     didProvideCredentials: _authService.loginWithCredentials,
                   )),
+                /// SignUpケース
                 if (snapshot.data!.authFlowStatus == AuthFlowStatus.signUp)
                   MaterialPage(child: SignUpPage(
                     shouldShowLogin: _authService.showLogin,
                     didProvideCredentials: _authService.signUpWithCredentials,
                   )),
+                /// verificationケース
                 if (snapshot.data!.authFlowStatus == AuthFlowStatus.verification)
                   MaterialPage(
                     child: VerificationPage(didProvideVerificationCode: _authService.verifyCode)
                   ),
+                /// logOutケース
+                if (snapshot.data!.authFlowStatus == AuthFlowStatus.session)
+                  MaterialPage(
+                    child: CameraFlow(shouldLogOut: _authService.logOut)
+                  )
               ],
               onPopPage: (route, result) => route.didPop(result),
             );
