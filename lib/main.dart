719b788fa3291f3cf05c7ba19b84f54d2f134da7
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-10-26 16:27:45 +0900

    feat(aws): imprement amplify auth cognito

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:flutter/material.dart';
 import 'package:lil_wardrobe/amplifyconfiguration.dart';
 import 'package:lil_wardrobe/services/auth_service.dart';
@@ -17,7 +18,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   /// AuthServiceのインスタンスを作成
   final _authService = AuthService();
 
@@ -38,54 +38,62 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Photo Gallery App',
-      theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
-      home: StreamBuilder<AuthState>(
-        /// authStateControllerからAuthStateストリームにアクセス
-        stream: _authService.authStateController.stream,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Navigator(
-              pages: [
-                /// Loginケース
-                if (snapshot.data!.authFlowStatus == AuthFlowStatus.login)
-                  MaterialPage(child: LoginPage(
-                    shouldShowSignUp: _authService.showSignUp,
-                    didProvideCredentials: _authService.loginWithCredentials,
-                  )),
-                /// SignUpケース
-                if (snapshot.data!.authFlowStatus == AuthFlowStatus.signUp)
-                  MaterialPage(child: SignUpPage(
-                    shouldShowLogin: _authService.showLogin,
-                    didProvideCredentials: _authService.signUpWithCredentials,
-                  )),
-                /// verificationケース
-                if (snapshot.data!.authFlowStatus == AuthFlowStatus.verification)
-                  MaterialPage(
-                    child: VerificationPage(didProvideVerificationCode: _authService.verifyCode)
-                  ),
-                /// logOutケース
-                if (snapshot.data!.authFlowStatus == AuthFlowStatus.session)
-                  MaterialPage(
-                    child: CameraFlow(shouldLogOut: _authService.logOut)
-                  )
-              ],
-              onPopPage: (route, result) => route.didPop(result),
-            );
-          } else {
-            /// streamにデータがない場合circularを表示
-            return Container(
-              alignment: Alignment.center,
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      )
-    );
+        title: 'Photo Gallery App',
+        theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
+        home: StreamBuilder<AuthState>(
+          /// authStateControllerからAuthStateストリームにアクセス
+          stream: _authService.authStateController.stream,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Navigator(
+                pages: [
+                  /// Loginケース
+                  if (snapshot.data!.authFlowStatus == AuthFlowStatus.login)
+                    MaterialPage(
+                        child: LoginPage(
+                      shouldShowSignUp: _authService.showSignUp,
+                      didProvideCredentials: _authService.loginWithCredentials,
+                    )),
+
+                  /// SignUpケース
+                  if (snapshot.data!.authFlowStatus == AuthFlowStatus.signUp)
+                    MaterialPage(
+                        child: SignUpPage(
+                      shouldShowLogin: _authService.showLogin,
+                      didProvideCredentials: _authService.signUpWithCredentials,
+                    )),
+
+                  /// verificationケース
+                  if (snapshot.data!.authFlowStatus ==
+                      AuthFlowStatus.verification)
+                    MaterialPage(
+                        child: VerificationPage(
+                            didProvideVerificationCode:
+                                _authService.verifyCode)),
+
+                  /// logOutケース
+                  if (snapshot.data!.authFlowStatus == AuthFlowStatus.session)
+                    MaterialPage(
+                        child: CameraFlow(shouldLogOut: _authService.logOut))
+                ],
+                onPopPage: (route, result) => route.didPop(result),
+              );
+            } else {
+              /// streamにデータがない場合circularを表示
+              return Container(
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ));
   }
 
   /// Amplifyを設定する関数
   void _configureAmplify() async {
+    /// 認証プラグイン
+    _amplify.addPlugin(authPlugins: [AmplifyAuthCognito()]);
+
     try {
       await _amplify.configure(amplifyconfig);
       print("configure Amplify");
