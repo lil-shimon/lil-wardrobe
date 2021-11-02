f0efb3810db6440ad45ba9d6c8d4c25e6e512332
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-11-02 11:39:32 +0900

    feat(s3): amplify add storage

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
-import 'package:flutter/material.dart';
 import 'package:lil_wardrobe/models/auth_credentials.dart';
 
 ///認証フローとして4つの段階がある
